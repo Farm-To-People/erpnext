75a49e63760720a088ce74f6e2488cf153b25e02
Brian Pond <brian@datahenge.com>
2024-04-02 10:02:09 -0700

    Remove item_filter code from this branch; too soon!

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -196,7 +196,7 @@ class Item(WebsiteGenerator):
 		# Late imports due to cross-module dependency:
 		from ftp.ftp_invent.redis.api import try_update_redis_inventory
 		from ftp.ftp_invent.redis.item_attributes import rewrite_attributes_by_item
-		from ftp.ftp_module.doctype.item_filter_map import update_filters_in_redis
+		# from ftp.ftp_module.doctype.item_filter_map import update_filters_in_redis
 
 		from ftp.sanity import update_sanity_by_item_code  # LEGACY Sanity
 		from ftp.ftp_sanity.product import js_update_sanity_product
@@ -205,7 +205,7 @@ class Item(WebsiteGenerator):
 		# Farm To People: Update redis after Item is modified.
 		try_update_redis_inventory(self.item_code)  # update Redis after Item is modified.
 		rewrite_attributes_by_item(self.item_code)  # Update the semi-static Redis data
-		update_filters_in_redis(self)
+		# update_filters_in_redis(self)
 
 		# Sanity Updates
 		update_sanity_by_item_code(self.item_code)  # LEGACY
