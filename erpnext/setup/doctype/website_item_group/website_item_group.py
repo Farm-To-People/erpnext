160a84bb7f0033c46e08b3bc64660a960da82112
Brian Pond <brian@datahenge.com>
2025-05-07 09:54:05 -0700

    Changes to Item Groups, based on Sanity Refactor

--- erpnext/setup/doctype/website_item_group/website_item_group.py
+++ erpnext/setup/doctype/website_item_group/website_item_group.py
@@ -22,10 +22,8 @@ class WebsiteItemGroup(Document):
 		parenttype: DF.Data
 	# end: auto-generated types
 
-	pass
-
 
 def on_doctype_update():
-	# Farm To People: Do not allow the same Item Group to be assigned twice (bug in standard code)
+	# Datahenge: Do not allow the same Item Group to be assigned twice (bug in standard code)
 	frappe.db.add_index("Website Item Group", ["parent", "parenttype", "item_group"])
 	frappe.db.add_unique("Website Item Group", ["parent", "parenttype", "item_group"])
