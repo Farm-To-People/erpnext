53440d5ada3a1e6ea56ddd5931266e3479d472d5
okeke emmanuel <85162261+Montego-arch@users.noreply.github.com>
2026-03-03 21:15:02 +0100

    358 never sub me (#11)
    * Adding cannot edit check on customer group
    
    * Adding never sub me check on customer
    
    ---------
    
    Co-authored-by: Sergey Bershadsky <929361+sergio-bershadsky@users.noreply.github.com>

--- erpnext/setup/doctype/customer_group/customer_group.py
+++ erpnext/setup/doctype/customer_group/customer_group.py
@@ -20,11 +20,11 @@ class CustomerGroup(NestedSet):
 		from ftp.ftp_module.doctype.transactional_email_override.transactional_email_override import TransactionalEmailOverride
 
 		accounts: DF.Table[PartyAccount]
+		cannot_edit_orders: DF.Check
 		cannot_subscribe_to_totes: DF.Check
 		credit_limits: DF.Table[CustomerCreditLimit]
 		customer_group_name: DF.Data
 		customer_name_suffix: DF.Data | None
-		default_language: DF.Link | None
 		default_price_list: DF.Link | None
 		default_shipping_rule: DF.Link | None
 		hdwd_behavior: DF.Literal["Normal", "Without Short-Sub", "Never"]
